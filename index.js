357b2083cc0e7f50ea2c6cb525f9c9ae092affb6
aabssmc <53542804+aabssmc@users.noreply.github.com>
2023-10-22 11:31:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import Jimp from "jimp";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1234;
 
 app.use(express.json());
 
